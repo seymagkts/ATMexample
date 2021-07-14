f7f4a45869eedebb7ceddcea7d736888b5e292ba
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-07-15 01:50:21 +0300

    Create start.java

---
+++ start.java
@@ -0,0 +1,7 @@
+public class b {
+        public static void main (String[]args) {
+            atm atm=new atm();
+            hesap hesap=new hesap("SGOKTAS","963",1547);
+            atm.calis(hesap);
+        }
+   }
